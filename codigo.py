8b667fdc76a4b7bfc79261371284cdc778a3c709
Adrian Clarck <adrian.clarck@aluno.senai.br>
2025-04-07 13:59:51 -0300

    Branch completa mas incorreta
    Adiciona código completo com erros para correção

--- codigo.py
+++ codigo.py
@@ -1,10 +1,18 @@
 def saudacao(nome):
-    if nome == "Ana":
+    if idade == "Ana": 
         print("Olá, Ana! Bem-vinda.")
-    elif nome == "Carlos":
+    elif nome = "Carlos":  
         print("E aí Carlos, beleza?")
+    else
+        print("Oi, prazer!")  
+
+def pedir_idade()
+    idade = input("Quantos anos você tem?)
+    if idade < "18":
+        print("Menor de idade.")
     else:
-        print("Oi, prazer!")
+        print("Maior de idade.")
 
 nome_usuario = input("Digite seu nome: ")
 saudacao(nome_usuario)
+pedir_idade()
